a13087d1e127b3bb64ca0587e6fcf248abec4353
Pjeschko16 <pjeschko@student.tgm.ac.at>
2026-02-18 11:45:22 +0100

    Creating the classes

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // --- TEST 1: Number Stack (Integer) ---
+        System.out.println("--- Testing Integer Stack ---");
+        Stack<Integer> numStack = new Stack<>(3);
+
+        try {
+            numStack.push(10); // Adding 10
+            numStack.push(20); // Adding 20
+            numStack.push(30); // Adding 30
+            System.out.println("Current Stack: " + numStack.list());
+
+            System.out.println("Peek: " + numStack.peek()); // Should be 30
+            System.out.println("Pop: " + numStack.pop());   // Removes 30
+            System.out.println("After Pop: " + numStack.list());
+
+            // Testing Exception: StackFull
+            numStack.push(40);
+            numStack.push(50); // This should trigger Exception
+        } catch (Exception e) {
+            System.out.println("Caught Expected Error: " + e.getMessage());
+        }
+
+        // --- TEST 2: Text Stack (String) ---
+        System.out.println("\n--- Testing String Stack ---");
+        Stack<String> textStack = new Stack<>(5);
+
+        try {
+            textStack.push("HTL");
+            textStack.push("Anichstraße");
+            textStack.push("Innsbruck");
+            System.out.println("Stack Content: " + textStack.list());
+
+            // Emptying the stack to test StackEmptyException
+            textStack.pop();
+            textStack.pop();
+            textStack.pop();
+            textStack.pop(); // This should trigger Exception
+        } catch (StackEmptyException e) {
+            System.out.println("Caught Expected Error: " + e.getMessage());
+        } catch (StackFullException e) {
+            System.out.println("Unexpected Error: " + e.getMessage());
         }
     }
 }
